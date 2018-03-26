3bffbb3ec0c3234dbbdf30aa41145e6268ad88bd
Fryann <fryann14@hotmail.com>
2018-03-25 23:13:49 -0400

    ejercicio 4  Solo falta diseno

--- js/Javascript.js
+++ js/Javascript.js
@@ -89,3 +89,28 @@ var m = {
 
 m.inicio();
 m.teclado();
+
+
+function transformar(){
+var num1,denom1,entero1,Resultado;
+entero1=entero.value;
+num1=parseInt (numerador.value);
+denom1= parseInt(denominador.value);
+if (denom1>0){
+resultado=(entero1*denom1)+ parseInt(num1);
+numeradore.value=resultado;
+denominadore.value=denom1;
+alert("El numero mixto ha sido transformado Correctamente... ");
+}
+else{
+alert("El denominador debe ser distino de cero (0)... ");
+}
+}
+function limpiar(){
+entero.value="";
+numerador.value="";
+denominador.value="";
+numeradore.value="";
+denominadore.value="";
+alert("Borrado de pantalla OK... ");
+}
